420e90d653f98a0eedeb126dc1c239723a36e86e
Efrainq07 <efrainq07@hotmail.com>
2022-08-29 04:27:01 -0500

    [FrontEnd] Se añadieron elementos de Chakra UI y AntDesign, se agrega nuevo diseño de barra de navegacion.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Box, Image, Link, Button, HStack ,StackDivider} from '@chakra-ui/react';
+import { Layout } from 'antd';
+import './App.css'
+
+const { Content, Sider, Footer, Header } = Layout;
 
 const App = () => {
     return (
-        <h1>
-            Heptagon
-        </h1>
+        <Layout>
+            <Layout>
+                <Header id='hepHeader'>
+                    <Image boxSize='100px' objectFit='cover' src='./heptagon.png' id='hepLogo'/>
+                    <HStack divider={<StackDivider borderColor='gray.200' />} h='25px' p={4} id='hepLinkContainer'>
+                        <Link className='hepHeaderLink'>
+                            Home
+                        </Link>
+                        <Link className='hepHeaderLink'>
+                            Explore
+                        </Link>
+                        <Link className='hepHeaderLink'>
+                            Contact
+                        </Link>
+                    </HStack>
+                    <Button colorScheme='purple' id='hepWalletButton'>Connect Wallet</Button>
+                </Header>
+            </Layout>
+        </Layout>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import {createRoot} from 'react-dom/client';
 import React,{StrictMode} from 'react';
 import { HashRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 
 createRoot(document.getElementById("app")).render(
     <StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <ChakraProvider>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </ChakraProvider>
     </StrictMode>
 );
